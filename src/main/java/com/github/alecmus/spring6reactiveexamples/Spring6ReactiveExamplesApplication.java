2bb5327decf7f3a4ce5146b0215aeb00f749bf7d
Alec Musasa <alecmus@live.com>
2023-06-26 16:39:45 +0200

    Added Spring Initializr code

---
+++ src/main/java/com/github/alecmus/spring6reactiveexamples/Spring6ReactiveExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.github.alecmus.spring6reactiveexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6ReactiveExamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring6ReactiveExamplesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/alecmus/spring6reactiveexamples/Spring6ReactiveExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.alecmus.spring6reactiveexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6ReactiveExamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
